cd9127d2a56c4ddc8090072f065458df6736d645
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-08 15:56:53 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+// server.js
+// where your node app starts
+
+// init project
 var express = require('express');
 var app = express();
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200})); 
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-//use stylesheet in public folder
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-//display html file in views director
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-//no date endpoint
 app.get("/api/", function (request, response) {
   let date = new Date();
   dateArray = date.toString().split("+");
@@ -24,39 +29,53 @@ app.get("/api/", function (request, response) {
     "utc" : `${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`
   })
 });
-
-//test API endpoint
+// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
-//given date endpoint
 app.get("/api/:date?", function (request, response) {
-  let dateArray = request.params.date.split("-");
-  let date_string = [dateArray[0], dateArray[1], dateArray[2]].join(" ");
-  
-  let date = new Date(date_string);
-
-  if(date ==  "Invalid Date"){
-    response.json({
-      error : "Invalid Date" 
-    })
-  }
-  else{
-    //date = new Date(2015, 12, 10);
-    dateArray.length = 0;
+  let date_string;
+  let date;
+  let dateArray;
+  if(!request.params.date.includes("-")){ //if its not a date
+    date_string = parseInt(request.params.date);
+    date = new Date(date_string);
     dateArray = date.toString().split("+");
-    dateArray = dateArray[0].split(" ");
-    //console.log(dateArray);
-    //console.log(`${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`)
-  
+  dateArray = dateArray[0].split(" ");
+
     response.json({
-      "unix" : date.getTime(),
-      "utc" : `${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`
-    })
+    "unix" : date_string,
+    "utc" : `${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`
+    }) 
+  }
+  else{ //its a date
+    let dateArray = request.params.date.split("-");
+    date_string = [dateArray[0], dateArray[1], dateArray[2]].join(" ");
+    
+    date = new Date(date_string);
+  
+    if(date ==  "Invalid Date"){
+      response.json({
+        error : "Invalid Date" 
+      })
+    }
+    else{
+      //date = new Date(2015, 12, 10);
+      dateArray.length = 0;
+      dateArray = date.toString().split("+");
+      dateArray = dateArray[0].split(" ");
+      //console.log(dateArray);
+      //console.log(`${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`)
+    
+      response.json({
+        "unix" : date.getTime(),
+        "utc" : `${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`
+      })
+    } 
   }
   
 });
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
