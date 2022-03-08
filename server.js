6c7f8c77d33b9b950ed9bbf41e37d3d1c098b744
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-08 14:42:34 -0800

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,16 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/:date?", function (request, response) {
   let dateArray = request.params.date.split("-");
   let date = new Date(dateArray[0], dateArray[1]-1, dateArray[2]);
-  
+  //date = new Date(2015, 12, 10);
+  dateArray.length = 0;
+  dateArray = date.toString().split("+");
+  dateArray = dateArray[0].split(" ");
+  console.log(dateArray);
+  console.log(`${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`)
   
   response.json({
-    "unix" : date.getTime()
+    "unix" : date.getTime(),
+    "utc" : `${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`
   })
   
 });
