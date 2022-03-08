e3c7a7f38b3d8432c6f5f94d6d03c88486a5c76f
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-08 14:52:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,33 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200})); 
 
+//use stylesheet in public folder
 app.use(express.static('public'));
 
+//display html file in views director
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//no date endpoint
+app.get("/api/", function (request, response) {
+  let date = new Date();
+  dateArray = date.toString().split("+");
+  dateArray = dateArray[0].split(" ");
+  console.log(dateArray);
+  
+  response.json({
+    "unix" : date.getTime(),
+    "utc" : `${dateArray[0]}, ${dateArray[2]} ${dateArray[1]} ${dateArray[3]} ${dateArray[4]} ${dateArray[5]}`
+  })
+});
+
+//test API endpoint
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//given date endpoint
 app.get("/api/:date?", function (request, response) {
   let dateArray = request.params.date.split("-");
   let date = new Date(dateArray[0], dateArray[1]-1, dateArray[2]);
