ef6d2bbd2dbbb51482f48603a84be5cc81e97f1b
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-08 14:42:01 -0800

    deliver UNIX time in JSON

--- server.js
+++ server.js
@@ -13,3 +13,14 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.get("/api/:date?", function (request, response) {
+  let dateArray = request.params.date.split("-");
+  let date = new Date(dateArray[0], dateArray[1]-1, dateArray[2]);
+  
+  
+  response.json({
+    "unix" : date.getTime()
+  })
+  
+});
